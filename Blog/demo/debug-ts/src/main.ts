a9ec256ed02318333a052e0c1125a60b799f801d
kerwin <kerwin.leeyi@gmail.com>
2021-08-24 22:05:07 +0800

    change authentication

--- Blog/demo/debug-ts/src/main.ts
+++
@@ -1,3 +0,0 @@
-console.log(__dirname);
-console.log(__filename);
-console.log(process.cwd());
--- Blog/python/subprocess_demo.py
+++
@@ -1,6 +0,0 @@
-# 使用subprocess管理子进程，可以用于启动电脑上的应用/打开文件等操作
-import subprocess
-
-if __name__ == '__main__':
-    proc = subprocess.Popen(['open', '/Applications/Apifox.app'])
-    proc.communicate()
