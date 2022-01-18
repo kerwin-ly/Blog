2496f89e486cdd7aad584b2321f5aac4ecf2d385
kerwin <kerwin.leeyi@gmail.com>
2022-01-18 23:43:25 +0800

    数组-二分法

--- demo/algorithm/index.js
+++ demo/algorithm/index.js
@@ -1,55 +1,141 @@
-// var lengthOfLongestSubstring = function (s) {
-//   // 使用滑动窗口处理
-//   if (!s.length) {
-//     return 0;
-//   }
-//   // let left = 0;
-//   let right = -1; // 从-1开始，指针右移
-//   let max = 0;
-//   const map = new Map();
-//   for (let i = 0; i < s.length; i++) {
-//     // left = i; // 左指针右移
-//     // if (map.get(s[i])) {
-//     //   // 如果左指针移动中，发现值重复，则删除该值，继续移动左指针
-//     //   map.delete(s[i]);
-//     // }
-//     if (i !== 0) {
-//       map.delete(s[i - 1]);
-//     }
-//     while (!map.get(s[right + 1]) && right + 1 < s.length) {
-//       map.set(s[right + 1], (right + 1).toString());
-//       right++; // 指针右移
-//     }
+// // var searchRange = function (nums, target) {
+// //   const getLeftBorder = (nums, target) => {
+// //     let left = 0,
+// //       right = nums.length - 1;
+// //     let leftBorder = -2; // 记录一下leftBorder没有被赋值的情况
+// //     while (left <= right) {
+// //       let middle = left + ((right - left) >> 1);
+// //       if (target <= nums[middle]) {
+// //         // 寻找左边界，nums[middle] == target的时候更新right
+// //         right = middle - 1;
+// //         leftBorder = right;
+// //       } else {
+// //         left = middle + 1;
+// //       }
+// //     }
+// //     return leftBorder;
+// //   };
 
-//     max = Math.max(max, right - i + 1);
-//   }
-//   return max;
+// //   const getRightBorder = (nums, target) => {
+// //     let left = 0,
+// //       right = nums.length - 1;
+// //     let rightBorder = -2; // 记录一下rightBorder没有被赋值的情况
+// //     while (left <= right) {
+// //       let middle = left + ((right - left) >> 1);
+// //       if (target < nums[middle]) {
+// //         right = middle - 1;
+// //       } else {
+// //         // 寻找右边界，nums[middle] == target的时候更新left
+// //         left = middle + 1;
+// //         rightBorder = left;
+// //       }
+// //     }
+// //     return rightBorder;
+// //   };
+
+// //   let leftBorder = getLeftBorder(nums, target);
+// //   let rightBorder = getRightBorder(nums, target);
+// //   // 情况一
+// //   if (leftBorder === -2 || rightBorder === -2) return [-1, -1];
+// //   // 情况三
+// //   if (rightBorder - leftBorder > 1) return [leftBorder + 1, rightBorder - 1];
+// //   // 情况二
+// //   return [-1, -1];
+// // };
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+// var searchRange = function (nums, target) {
+//   const getLeftBorder = (nums, target) => {
+//     let left = 0;
+//     let right = nums.length - 1;
+//     let leftBorder = null;
+//     let mid;
+//     let pivot;
+//     while (left <= right) {
+//       mid = Math.floor((right - left) / 2) + left;
+//       pivot = nums[mid];
+
+//       if (target < pivot) {
+//         right = mid - 1;
+//       } else if (target > pivot) {
+//         left = mid + 1;
+//       } else {
+//         right = mid - 1;
+//         // 由于是获取左边界，将右边界固定，来缩小左边界范围
+//         leftBorder = mid;
+//       }
+//     }
+//     if (leftBorder === null) return -1;
+//     return leftBorder;
+//   };
+//   const getRightBorder = (nums, target) => {
+//     let left = 0;
+//     let right = nums.length - 1;
+//     let rightBorder = null;
+//     let mid;
+//     let pivot;
+//     while (left <= right) {
+//       mid = Math.floor((right - left) / 2) + left;
+//       pivot = nums[mid];
+
+//       if (target < pivot) {
+//         right = mid - 1;
+//       } else if (target > pivot) {
+//         left = mid + 1;
+//       } else {
+//         left = mid + 1;
+//         // 由于是获取右边界，将左边界固定，来缩小右边界范围
+//         rightBorder = mid;
+//       }
+//     }
+//     if (rightBorder === null) return -1;
+//     return rightBorder;
+//   };
+//   const leftBorder = getLeftBorder(nums, target);
+//   const rightBorder = getRightBorder(nums, target);
+//   return [leftBorder, rightBorder];
 // };
-// lengthOfLongestSubstring('aab');
+
 /**
  * @param {number[]} nums
  * @param {number} target
- * @return {number}
+ * @return {number[]}
  */
- var search = function(nums, target) {
-  // 1. 取数组中间的值pivot与target进行比较
-  // 2. 如果target小于pivot，则说明target在数组左侧；如果target大于pivot，则说明target在数组右侧
-  // 3. 重复第二步，直到target等于pivot 或 nums.length === 1，否则输出-1
-  let left = 0;
-  let right = nums.length - 1;
+var searchRange = function (nums, target) {
+  const getBorder = (nums, target, type) => {
+    let left = 0;
+    let right = nums.length - 1;
+    let border = null;
+    while (left <= right) {
+      const mid = Math.floor((right - left) / 2) + left;
+      const pivot = nums[mid];
 
-  if (left <= right) {
-      let index = Math.floor((right - left) / 2) + left;
-      let pivot = nums[index];
       if (target < pivot) {
-          right = index - 1;
+        right = mid - 1;
       } else if (target > pivot) {
-          left = index + 1;
+        left = mid + 1;
       } else {
-          return index;
+        // 如果target === pivot
+        if (type === 'left') {
+          // 如果寻找左边界值，则将右指针左移，缩小左侧数组范围
+          right = mid - 1;
+          border = mid;
+        } else {
+          // 如果寻找右边界值，则将左指针右移，缩小右侧数组范围
+          left = mid + 1;
+          border = mid;
+        }
       }
-      // return -1;
-  }
-};
+    }
+    if (border === null) return -1;
+    return border;
+  };
 
-search([-1, 0, 3, 5, 9, 12], 9)
+  const leftBorder = getBorder(nums, target, 'left');
+  const rightBorder = getBorder(nums, target, 'right');
+  return [leftBorder, rightBorder];
+};
+searchRange([5, 7, 7, 8, 8, 9], 8);
