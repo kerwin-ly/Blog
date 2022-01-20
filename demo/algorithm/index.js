3fc5a322628fc813b782ecc53d2e69734247230b
kerwin <kerwin.leeyi@gmail.com>
2022-01-21 00:10:21 +0800

    数组-双指针

--- demo/algorithm/index.js
+++ demo/algorithm/index.js
@@ -1,12 +1,25 @@
-function removeElement(nums, val) {
-  let slowIndex = 0,
-    fastIndex = 0;
-  while (fastIndex < nums.length) {
-    if (nums[fastIndex] !== val) {
-      nums[slowIndex++] = nums[fastIndex];
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function (target, nums) {
+  let left = -1;
+  let right = 0;
+  let sum = 0;
+  let minLen = null;
+  while (left <= right && right === nums.length - 1) {
+    nums[left] = typeof nums[left] === undefined ? 0 : nums[left];
+    sum = sum + nums[left] + nums[right];
+    if (sum < target) {
+      right++;
+    } else {
+      if (right - left < minLen || minLen === null) {
+        minLen = right - left;
+      }
+      left++;
     }
-    fastIndex++;
   }
-  return slowIndex;
-}
-removeElement([0, 1, 2, 2, 2, 3, 2], 2);
+  return minLen === null ? 0 : minLen;
+};
+minSubArrayLen(7, [2, 3, 1, 2, 4, 3]);
