95be0a26332f8621ee66dc73c952479481f331a7
kerwin <kerwin.leeyi@gmail.com>
2022-01-25 23:51:20 +0800

    数组-水果成篮问题

--- demo/algorithm/algorithm-debug.js
+++ demo/algorithm/algorithm-debug.js
@@ -11,27 +11,40 @@ var totalFruit = function (fruits) {
   const map = new Map();
 
   while (right < fruits.length) {
+    // if (right - left + 1 > max) {
+    //   max = right - left + 1;
+    // }
     // sum++;
     if (!map.has(fruits[right])) {
-      map.set(fruits[right], true);
+      map.set(fruits[right], 1);
+    } else {
+      map.set(fruits[right], map.get(fruits[right]) + 1);
     }
-
+    right++;
     // sum++;
 
-    if (right - left + 1 > max) {
-      max = right - left + 1;
-    }
-
     while (map.size > MAX_LENGTH) {
-      map.delete(fruits[left]);
+      if (map.get(fruits[left])) {
+        map.set(fruits[left], map.get(fruits[left]) - 1);
+      }
+      // console.log(fruits[left] + '--' + map.get(fruits[left]));
+      // let leftkey = fruits[left];
+      if (map.get(fruits[left]) <= 0) {
+        map.delete(fruits[left]);
+      }
+
       // sum--;
       // 判断left的下一个水果是否在篮子中
       left++;
-      if (!map.has(fruits[left])) {
-        map.set(fruits[left], left.toString());
-      }
+      // if (!map.has(fruits[left])) {
+      //   map.set(fruits[left], left.toString());
+      // }
+    }
+    if (right - left > max) {
+      max = right - left;
     }
   }
   return max;
 };
 totalFruit([3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4]);
+// totalFruit([0, 1, 2, 2]);
