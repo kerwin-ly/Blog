1a38d01ec8eb810c1ff3937144c799dc2bc422a0
kerwin <kerwin.leeyi@gmail.com>
2022-02-17 23:10:35 +0800

    优化单调队列

--- demo/algorithm/algorithm-debug.js
+++ demo/algorithm/algorithm-debug.js
@@ -1,35 +1,23 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number[]}
- */
 var maxSlidingWindow = function (nums, k) {
-  const queue = []; // 单调队列，单调递减
+  const queue = []; // 单调递减队列，存储数组的下标
   const result = []; // 结果数组
 
-  let max = nums[0];
-  // 求出移动滑动窗口前，滑动窗口中的最大值
-  for (let i = 1; i < k; i++) {
-    if (max < nums[i]) {
-      max = nums[i];
-    }
-  }
-  queue.push(max);
-  result.push(max);
-
-  for (let i = k; i < nums.length; i++) {
-    let front = queue[0]; // 队列头元素
-    // 如果滑动窗口移除的元素刚好是上个滑动窗口的最大值，则将队列头部的值取出
-    if (nums[i - k] === front) {
-      queue.shift();
-    }
-    // 将队列尾部元素与当前元素一直比较，如果当前元素比队尾元素大，则移出队尾元素。直到当前元素比队尾元素小为止
-    while (queue[queue.length - 1] < nums[i]) {
-      queue.length--;
+  for (let i = 0; i < nums.length; i++) {
+    // 将队列尾部元素与当前元素比较，如果当前元素比队尾元素大，则移出队尾元素。直到当前元素比队尾元素小为止
+    // 这样做是为了保证队列的单调递减
+    while (queue.length && nums[queue[queue.length - 1]] <= nums[i]) {
+      queue.pop(); // 删除队尾元素
     }
     queue.push(nums[i]); // 将当前元素插入队尾
-    result.push(queue[0]); // 将最大元素放入结果中
+    // 如果滑动窗口区间的最左侧 大于等于 队头下标，则将队头删除
+    if (i - k >= queue[0]) {
+      queue.shift();
+    }
+    // 如果遍历i，已达到滑动窗口的尾部。说明一次滑动窗口区间的值已经处理完，向结果数组中添加当前窗口的最大值
+    if (i >= k - 1) {
+      result.push(nums[queue[0]]); // 将最大元素放入结果中
+    }
   }
   return result;
 };
-maxSlidingWindow([9, 10, 9, -7, -4, -8, 2, -6], 5);
+maxSlidingWindow([3, 3, 5, 5, 6, 7], 3);
